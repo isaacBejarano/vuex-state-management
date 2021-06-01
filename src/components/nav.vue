bc438bb1cfdf595bc61a0c30950e3e2cfa22633f
isaacBejarano <isaacbejaranopro@gmail.com>
2021-06-01 17:59:27 +0200

    setup end

---
+++ src/components/nav.vue
@@ -0,0 +1,50 @@
+<template>
+	<nav>
+		<router-link :to="navLinks[0][1]">{{ navLinks[0][0] }}</router-link>
+		|
+		<router-link :to="navLinks[1][1]">{{ navLinks[1][0] }}</router-link>
+	</nav>
+</template>
+
+<script lang="ts">
+	import { ref } from "vue";
+
+	export default {
+		setup(): object {
+			// props
+			const navLinks = ref(<string[][]>[
+				["VUEX-driven", "/"],
+				["Event-driven", "/event-driven"],
+			]);
+
+			// DI
+			// const key: InjectionKey<string> = Symbol();
+			// provide(key, "foo");
+			// const foo: string | undefined = inject(key);
+
+			// const bar = inject<string | undefined>("bar");
+
+			// return props
+			return { navLinks };
+		},
+	};
+
+	// NOTE:
+	// We can force type inference by using defineComponent()
+	// I think, explcicid TS typing is more reliable and robust :)
+</script>
+
+<style lang="scss" scoped>
+	nav {
+		padding: 1rem;
+
+		a {
+			font-weight: bold;
+			color: #2c3e50;
+
+			&.router-link-exact-active {
+				color: #42b983;
+			}
+		}
+	}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,25 +1,23 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import Home from '../views/Home.vue'
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+
+import StateDriven from "../views/StateDriven.vue";
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+	{
+		path: "/",
+		name: "State Driven",
+		component: StateDriven,
+	},
+	{
+		path: "/event-driven",
+		name: "Event Driven",
+		component: () => import("../views/EventDriven.vue"), // lazy
+	},
+];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+	history: createWebHistory(process.env.BASE_URL),
+	routes,
+});
 
-export default router
+export default router;

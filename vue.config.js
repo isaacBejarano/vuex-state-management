4495d4c78c844612dfeb90295db502248730d697
isaacBejarano <isaacbejaranopro@gmail.com>
2021-06-02 20:15:11 +0200

    deploy in Apache subfolder

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 module.exports = {
-  assetsDir: 'assets',
-  productionSourceMap: false
-}
+	assetsDir: "assets",
+	productionSourceMap: false,
+	// FIXME: remove to deploy in Apache root
+	publicPath: process.env.NODE_ENV === "production" ? "/vuex-counter/" : "/",
+};

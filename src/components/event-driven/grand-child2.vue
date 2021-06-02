533288d53b29ba76b98346410500b4a8f98c37be
isaacBejarano <isaacbejaranopro@gmail.com>
2021-06-02 02:36:08 +0200

    event-driven structure setup

---
+++ src/components/event-driven/grand-child2.vue
@@ -0,0 +1,41 @@
+<template>
+	<section class="grand-child2">
+		<h3>GrandChild Component</h3>
+		<h4>GrandChild State: {{ state3 }}</h4>
+
+		<Counter @emit-counter="counter(...$event)" :stateInit="state_init"></Counter>
+	</section>
+</template>
+
+<script lang="ts">
+	import { ref } from "vue";
+
+	import Counter from "../counter.vue";
+
+	export default {
+		name: "GrandChild2", // devtools
+		components: { Counter },
+		props: { stateInit: Number },
+		setup(props: any) {
+			// -> props
+			let state_init = ref(props.stateInit);
+
+			// attr
+			let state3 = ref(state_init);
+
+			// methods
+			function counter(e: Event, payload: number) {
+				state3.value = payload;
+			}
+
+			return { state3, state_init, counter };
+		},
+	};
+</script>
+
+<style lang="scss">
+	.grand-child2 {
+		padding: 1rem;
+		background-color: palevioletred;
+	}
+</style>
